f357bde8c5f072d8e782a9189eb51152e2d1a285
김동규 <pythakyu@gmail.com>
2022-12-16 01:05:53 +0900

    [ADD] 게시물 조회 엔드포인트

--- app.js
+++ app.js
@@ -30,6 +30,37 @@ const posts = [
     userId: 1,
   },
 ];
+const userPostData = [
+  {
+    userID: 1,
+    userName: "Rebekah Johnson",
+    postingId: 1,
+    postingTitle: "간단한 HTTP API 개발 시작!",
+    postingContent:
+      "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server를 구현.",
+  },
+  {
+    userID: 2,
+    userName: "Fabian Predovic",
+    postingId: 2,
+    postingTitle: "HTTP의 특성",
+    postingContent: "Request/Response와 Stateless!!",
+  },
+  {
+    userID: 3,
+    userName: "new user 1",
+    postingId: 3,
+    postingImageUrl: "내용 1",
+    postingContent: "sampleContent3",
+  },
+  {
+    userID: 4,
+    userName: "new user 2",
+    postingId: 4,
+    postingImageUrl: "내용 2",
+    postingContent: "sampleContent4",
+  },
+];
 
 const httpRequestListener = function (request, response) {
   const { url, method } = request;
@@ -41,6 +72,16 @@ const httpRequestListener = function (request, response) {
     } else if (url === "/posts") {
       response.writeHead(200, { "content-type": "application/json" });
       response.end(JSON.stringify({ message: "userCreated" }));
+    }
+  } else if (method === "GET") {
+    if (url === "/userPostData") {
+      let body = "";
+
+      request.on("data", (data) => {
+        body += data;
+      });
+      response.writeHead(200, { "content-type": "application/json" });
+      response.end(JSON.stringify({ data: userPostData }));
     }
   }
 };
